14adfff0691968b3ab15e7ae4cc628fa03e0a44f
arunkarthik132 <karthikarun172@gmail.com>
2020-12-28 09:10:53 +0530

    connected all funtions

---
+++ fundementalOperations.py
@@ -0,0 +1,22 @@
+
+
+def addition(a, b):
+    return a+b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a*b
+
+
+def division(a, b):
+    return a/b
+
+
+print(addition(2, 3))
+print(subtraction(5, 4))
+print(multiplication(2, 4))
+print(division(4, 4))
